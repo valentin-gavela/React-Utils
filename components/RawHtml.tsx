3b92e487eea8d2408ec34a4db8e00674f61e986c
valentin-gavela <valentin.gavela@despegar.com>
2022-04-21 17:45:19 -0300

    1 file modified

--- components/RawHtml.tsx
+++ components/RawHtml.tsx
@@ -1,5 +1,7 @@
+// @ts-nocheck
+
 import { sanitize } from 'dompurify';
-import React, { ReactHTML } from 'react';
+import React, { ReactHTML, useRef } from 'react';
 
 const sanitizeInnerHtml = (input: string | React.ReactNode) => ({
   dangerouslySetInnerHTML: {
@@ -7,13 +9,50 @@ const sanitizeInnerHtml = (input: string | React.ReactNode) => ({
   },
 });
 
-export const Raw: React.FC<Props> = ({ t = 'p', className, children }) => {
+const useRemoveElementKeepingChildren = () => {
+  const ref = useRef();
+
+  const remove = (r: React.MutableRefObject) => {
+    ref.current = r;
+
+    if (!r) return Promise.resolve();
+
+    ref.current.childNodes.forEach((node) =>
+      ref.current.parentElement.appendChild(node)
+    );
+    ref.current.remove();
+
+    return Promise.resolve();
+  };
+
+  return { remove };
+};
+
+/**
+ * This component allows the safe creation of an htmlString element
+ * @param t If you ignore this param, we just add htmlString without
+ * wrapping the string. Ignore `t` if htmlString has htmlElement container, for example:
+ * `<div class="red">Hello I'm red</div>`
+ */
+export const Raw: React.FC<Props> = ({ t, className, children }) => {
+  const { remove } = useRemoveElementKeepingChildren();
+
   return React.useMemo(() => {
-    return React.createElement(t, {
+    /**
+     * Si no se le ingresa el "t" como param,
+     * agregamos una 'p' para tener un parent en el DOM */
+    return React.createElement(t || 'p', {
       className,
       ...sanitizeInnerHtml(children),
+      ref: async (r) => {
+        if (!t) {
+          /**
+           * Luego removemos el padre dejando los hijos */
+          await remove(r);
+        }
+      },
     });
-  }, [children, className, t]);
+  }, [children, className, remove, t]);
 };
 
 type Props = {
